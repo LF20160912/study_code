b8329536d61a6aa53a9ed94241d0d09890b9e701
LF20160912 <2576731178@qq.com>
2022-02-22 23:14:58 +0800

    get configuration from remote response

--- cloud-provider/src/main/java/com/example/cloudprovider/CloudProviderApplication.java
+++ cloud-provider/src/main/java/com/example/cloudprovider/CloudProviderApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.example.cloudprovider.mapper")
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class CloudProviderApplication {
 
     public static void main(String[] args) {
