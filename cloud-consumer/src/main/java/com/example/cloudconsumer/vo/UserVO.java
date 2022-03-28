c6b79f1ea63656ef5cbccce27a0a4968f4621d9d
LF20160912 <2576731178@qq.com>
2022-03-28 23:31:14 +0800

    get configuration from remote response

--- cloud-consumer/src/main/java/com/example/cloudconsumer/vo/UserVO.java
+++ cloud-consumer/src/main/java/com/example/cloudconsumer/vo/UserVO.java
@@ -1,5 +1,7 @@
 package com.example.cloudconsumer.vo;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,6 +11,8 @@ import java.util.Date;
 @Setter
 public class UserVO {
     private Integer userId;
-    private String userName;
+    @JsonAlias("userName")//映射别名
+    private String Name;
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date userBirth;
 }
--- cloud-provider/src/main/java/com/example/cloudprovider/CloudProviderApplication.java
+++ cloud-provider/src/main/java/com/example/cloudprovider/CloudProviderApplication.java
@@ -3,11 +3,9 @@ package com.example.cloudprovider;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.example.cloudprovider.mapper")
-@EnableDiscoveryClient
 public class CloudProviderApplication {
 
     public static void main(String[] args) {
